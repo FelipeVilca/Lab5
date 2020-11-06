f767a53bdcd7b2dbe031ad06cb88e4fe5308df3f
Felipe Vilca <felipe.vilca87@gmail.com>
2020-11-06 19:54:28 +0100

    Initial commit

---
+++ WebScraper/WebScraper.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WebScraper
+{
+    partial class WebScraper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLink = new System.Windows.Forms.TextBox();
+            this.btnExtract = new System.Windows.Forms.Button();
+            this.txtFoundLinks = new System.Windows.Forms.TextBox();
+            this.btnSaveImages = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtLink
+            // 
+            this.txtLink.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLink.Location = new System.Drawing.Point(13, 13);
+            this.txtLink.Name = "txtLink";
+            this.txtLink.Size = new System.Drawing.Size(775, 20);
+            this.txtLink.TabIndex = 0;
+            this.txtLink.Text = "http://gp.se";
+            // 
+            // btnExtract
+            // 
+            this.btnExtract.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExtract.Location = new System.Drawing.Point(683, 39);
+            this.btnExtract.Name = "btnExtract";
+            this.btnExtract.Size = new System.Drawing.Size(105, 23);
+            this.btnExtract.TabIndex = 1;
+            this.btnExtract.Text = "Extract";
+            this.btnExtract.UseVisualStyleBackColor = true;
+            this.btnExtract.Click += new System.EventHandler(this.btnExtract_Click);
+            // 
+            // txtFoundLinks
+            // 
+            this.txtFoundLinks.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFoundLinks.Location = new System.Drawing.Point(13, 68);
+            this.txtFoundLinks.Multiline = true;
+            this.txtFoundLinks.Name = "txtFoundLinks";
+            this.txtFoundLinks.Size = new System.Drawing.Size(775, 341);
+            this.txtFoundLinks.TabIndex = 2;
+            // 
+            // btnSaveImages
+            // 
+            this.btnSaveImages.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSaveImages.Location = new System.Drawing.Point(683, 415);
+            this.btnSaveImages.Name = "btnSaveImages";
+            this.btnSaveImages.Size = new System.Drawing.Size(105, 23);
+            this.btnSaveImages.TabIndex = 3;
+            this.btnSaveImages.Text = "Save Images";
+            this.btnSaveImages.UseVisualStyleBackColor = true;
+            this.btnSaveImages.Click += new System.EventHandler(this.btnSaveImages_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblCount.AutoSize = true;
+            this.lblCount.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount.ForeColor = System.Drawing.Color.Red;
+            this.lblCount.Location = new System.Drawing.Point(12, 419);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(79, 19);
+            this.lblCount.TabIndex = 4;
+            this.lblCount.Text = "Found: 0";
+            // 
+            // WebScraper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.btnSaveImages);
+            this.Controls.Add(this.txtFoundLinks);
+            this.Controls.Add(this.btnExtract);
+            this.Controls.Add(this.txtLink);
+            this.Name = "WebScraper";
+            this.Text = "Web Scraper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLink;
+        private System.Windows.Forms.Button btnExtract;
+        private System.Windows.Forms.TextBox txtFoundLinks;
+        private System.Windows.Forms.Button btnSaveImages;
+        private System.Windows.Forms.Label lblCount;
+    }
+}
+
---
+++ WebScraper/WebScraper.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebScraper
+{
+    public partial class WebScraper : Form
+    {
+        #region Fields
+        /// <summary>
+        /// List of images found in html
+        /// </summary>
+        public static List<string> LastImagesCaptured;
+
+        /// <summary>
+        /// Last directory selected by user
+        /// </summary>
+        public static string LastFolderDirectory;
+
+        /// <summary>
+        /// HTTP Client to download images
+        /// </summary>
+        public static readonly HttpClient s_client = new HttpClient
+        {
+            MaxResponseContentBufferSize = 1_000_000
+        };
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public WebScraper()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Events
+        /// <summary>
+        /// Click event of extract button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void btnExtract_Click(object sender, EventArgs e)
+        {
+            await ExtractImagesAsync();
+        }
+
+        /// <summary>
+        /// Click event of save images button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void btnSaveImages_Click(object sender, EventArgs e)
+        {
+            if (LastImagesCaptured != null && LastImagesCaptured.Count > 0)
+            {
+                FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+                folderDlg.ShowNewFolderButton = true;
+                // Show the FolderBrowserDialog.  
+                DialogResult result = folderDlg.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    LastFolderDirectory = folderDlg.SelectedPath;
+                    Environment.SpecialFolder root = folderDlg.RootFolder;
+                    await DownloadImagesAsync();
+                }
+            }
+        }
+        #endregion
+        
+        #region Methods
+        /// <summary>
+        /// Find image tags from the input html
+        /// </summary>
+        /// <param name="inputHTML">input html</param>
+        /// <returns>List of urls of image tags</returns>
+        public static IEnumerable<String> GetImageLinks(String inputHTML)
+        {
+            const string pattern = @"<img\b[^\<\>]+?\bsrc\s*=\s*[""'](?<L>.+?)[""'][^\<\>]*?\>";
+
+            foreach (Match match in Regex.Matches(inputHTML, pattern, RegexOptions.IgnoreCase))
+            {
+                var imageLink = match.Groups["L"].Value;
+
+                yield return imageLink;
+            }
+        }
+
+        /// <summary>
+        /// Download HTML and then extract images tags from html
+        /// </summary>
+        /// <returns></returns>
+        public async Task ExtractImagesAsync()
+        {
+            try
+            {
+                string url = txtLink.Text;
+                btnExtract.Text = "Extracting..";
+                txtFoundLinks.Text = "";
+
+                btnExtract.Enabled = false;
+                HttpClient client = new HttpClient();
+                ServicePointManager.Expect100Continue = true;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+                HttpResponseMessage response = await client.GetAsync(url);
+                HttpContent content = response.Content;
+                string result = await content.ReadAsStringAsync();
+
+                var lst = GetImageLinks(result);
+
+                LastImagesCaptured = lst.ToList();
+
+                for (int i = 0; i < LastImagesCaptured.Count; i++)
+                {
+                    if (!LastImagesCaptured[i].StartsWith("http"))
+                    {
+                        LastImagesCaptured[i] = url + LastImagesCaptured[i];
+                    }
+                }
+
+                txtFoundLinks.Text = string.Join(Environment.NewLine, LastImagesCaptured);
+                lblCount.Text = "Found: " + LastImagesCaptured.Count;
+                btnExtract.Text = "Extract";
+                btnExtract.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                lblCount.Text = "Found: " + 0;
+                btnExtract.Text = "Extract";
+                btnExtract.Enabled = true;
+            }
+        }
+
+        /// <summary>
+        /// This will create list of tasks and then will exxecute each one in async mode and will download images
+        /// </summary>
+        /// <returns></returns>
+        public static async Task DownloadImagesAsync()
+        {
+            IEnumerable<Task> downloadTasksQuery =
+                from url in LastImagesCaptured
+                select ProcessUrlAsync(url, s_client);
+
+            List<Task> downloadTasks = downloadTasksQuery.ToList();
+
+            while (downloadTasks.Any())
+            {
+                Task finishedTask = await Task.WhenAny(downloadTasks);
+                if (finishedTask.Exception != null)
+                {
+                    //Exception
+                }
+
+                downloadTasks.Remove(finishedTask);
+            }
+
+            MessageBox.Show("Downloading Completed.", "Web Scrapper");
+        }
+
+        /// <summary>
+        /// This method will download individual image from its URL using http client
+        /// </summary>
+        /// <param name="url">url to download</param>
+        /// <param name="client">http client</param>
+        /// <returns></returns>
+        static async Task ProcessUrlAsync(string url, HttpClient client)
+        {
+            byte[] content = await client.GetByteArrayAsync(url);
+            Uri uri = new Uri(url);
+            string filename = "";
+            if (uri.IsFile)
+                filename = System.IO.Path.GetFileName(uri.LocalPath);
+            else
+                filename = Guid.NewGuid() + (url.EndsWith(".png") ? ".png" : ".jpg");
+
+            var fileFullPath = Path.Combine(LastFolderDirectory, filename);
+            using (FileStream fs = new FileStream(fileFullPath, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                await fs.WriteAsync(content, 0, content.Length);
+            }
+
+        }
+        #endregion
+
+    }
+}
